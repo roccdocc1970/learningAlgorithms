a62301103317b03d75d4cef961c0f4e52409ae92
unknown <roccdocc1970@comcast.net>
2018-01-11 03:01:00 -0500

    another pcommit

--- angularAlgorithms/src/app/app.module.ts
+++ angularAlgorithms/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LinkedListsComponent } from './linked-lists/linked-lists.component';
@@ -8,6 +8,12 @@ import { StacksAndCuesComponent } from './stacks-and-cues/stacks-and-cues.compon
 import { TreesAndGraphsComponent } from './trees-and-graphs/trees-and-graphs.component';
 import { ArraysAndStringsComponent } from './arrays-and-strings/arrays-and-strings.component';
 
+const appRoutes: Routes = [
+  { path: 'linked-lists', component: LinkedListsComponent },
+  { path: 'stacks-and-cues', component: StacksAndCuesComponent },
+  { path: 'trees-and-graphs', component: TreesAndGraphsComponent },
+  { path: 'arrays-and-strings', component: ArraysAndStringsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,6 +24,10 @@ import { ArraysAndStringsComponent } from './arrays-and-strings/arrays-and-strin
     ArraysAndStringsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
--- angularAlgorithms/src/app/arrays-and-strings/arrays-and-strings.component.ts
+++ angularAlgorithms/src/app/arrays-and-strings/arrays-and-strings.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-arrays-and-strings',
   templateUrl: './arrays-and-strings.component.html',
+  styleUrls: ['./arrays-and-strings.component.css']
 })
 export class ArraysAndStringsComponent implements OnInit {
 
